d17b3d6caa48c359ed83ef988b8370b4d9b8b2c2
Josef Kufner <josef@kufner.cz>
2016-02-19 14:26:51 +0100

    Import & clean up the example editor

---
+++ WebWrapEditor.pro
@@ -0,0 +1,11 @@
+QT += widgets uitools
+
+SOURCES = main.cpp mainwindow.cpp
+HEADERS = mainwindow.h
+RESOURCES = WebWrapEditor.qrc
+
+OBJECTS_DIR=build
+MOC_DIR=build
+RCC_DIR=build
+UI_DIR=build
+
---
+++ mainwindow.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2015  Josef Kufner <josef@kufner.cz>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+class QAction;
+class QMenu;
+class QPlainTextEdit;
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+	public:
+		MainWindow();
+
+	protected:
+		void closeEvent(QCloseEvent *event) Q_DECL_OVERRIDE;
+
+		private slots:
+			void newFile();
+		void open();
+		bool save();
+		bool saveAs();
+		void about();
+		void documentWasModified();
+
+	private:
+		void createActions();
+		void createMenus();
+		void createToolBars();
+		void createStatusBar();
+		void readSettings();
+		void writeSettings();
+		bool maybeSave();
+		void loadFile(const QString &fileName);
+		bool saveFile(const QString &fileName);
+		void setCurrentFile(const QString &fileName);
+		QString strippedName(const QString &fullFileName);
+
+		QPlainTextEdit *textEdit;
+		QString curFile;
+
+		QMenu *fileMenu;
+		QMenu *editMenu;
+		QMenu *helpMenu;
+		QToolBar *fileToolBar;
+		QToolBar *editToolBar;
+		QAction *newAct;
+		QAction *openAct;
+		QAction *saveAct;
+		QAction *saveAsAct;
+		QAction *exitAct;
+		QAction *cutAct;
+		QAction *copyAct;
+		QAction *pasteAct;
+		QAction *aboutAct;
+		QAction *aboutQtAct;
+};
+
+#endif
+
